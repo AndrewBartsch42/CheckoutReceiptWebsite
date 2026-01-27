56b81b3cf44dccfb62e7fefb3c2528043d0fd561
andrewbartsch42 <andrewbartsch01@gmail.com>
2026-01-27 15:17:44 -0600

    added the receipt alert print out and calculations

--- receiptScript.js
+++ receiptScript.js
@@ -6,7 +6,7 @@ function getElement(selector) {
 }
 
 function calculateButtonClick(event) {
-    let error_occured = true;
+    let error_occured = false;
     event.preventDefault();
     //reset of error text content fields
     customerErr.textContent = "*";
@@ -14,6 +14,7 @@ function calculateButtonClick(event) {
     priceErr.textContent = "*";
     quantityErr.textContent = "*";
     cashErr.textContent = "*";
+
     //inputs
     const customer_name = document.querySelector("#customer_name").value.trim();
     const grocery_item = document.querySelector("#grocery_item").value.trim();
@@ -23,10 +24,16 @@ function calculateButtonClick(event) {
     const unit_price = parseFloat(unit_price_Str);
     const quantity = parseInt(quantity_Str);
     const cash = parseFloat(cash_Str);
+    // constant rates and money function
+    const discount_rate = 0.10;
+    const tax_rate = 0.0825;
+    const money = (n) => n.toFixed(2);
+
+
 
     if (customer_name === "") {
         customerErr.textContent = "Required";
-        error_occured = false;
+        error_occured = true;
     }
     else {
         customerErr.textContent = "";
@@ -34,33 +41,33 @@ function calculateButtonClick(event) {
 
     if (grocery_item === ""){
         groceryErr.textContent = "Required";
-        error_occured = false;
+        error_occured = true;
     } else {
         groceryErr.textContent = "";
     }
 
     if (unit_price_Str === ""){
         priceErr.textContent = "Required";
-        error_occured = false;
+        error_occured = true;
     } else if (!Number.isFinite(unit_price)) {
         priceErr.textContent = "Must be numeric";
-        error_occured = false;
+        error_occured = true;
     } else if (unit_price < 0) {
         priceErr.textContent = "Must be greater than 0"
-        error_occured = false;
+        error_occured = true;
     } else {
         priceErr.textContent = "";
     }
 
     if (quantity_Str === ""){
         quantityErr.textContent = "Required";
-        error_occured = false;
+        error_occured = true;
     } else if (!Number.isFinite(quantity)) {
         quantityErr.textContent = "Must be numeric";
-        error_occured = false;
+        error_occured = true;
     } else if (quantity < 0){
         quantityErr.textContent = "Must be greater than 0";
-        error_occured - false;
+        error_occured - true;
     } else if (!Number.isInteger(quantity)){
         quantityErr.textContent = "Must be a whole number";
     } else {
@@ -69,15 +76,38 @@ function calculateButtonClick(event) {
 
     if (cash_Str === ""){
         cashErr.textContent = "Required";
-        error_occured = false;
+        error_occured = true;
     } else if (!Number.isFinite(cash)) {
         cashErr.textContent = "Must be numeric";
-        error_occured = false;
+        error_occured = true;
     } else if (cash < 0) {
         cashErr.textContent = "Must be greater than 0"
-        error_occured = false;
+        error_occured = true;
     } else {
         cashErr.textContent = "";
+    }
+
+    if( error_occured == false){
+        let subtotal = quantity * unit_price
+        let discounted_amount = (subtotal >= 50) ? subtotal * discount_rate : 0;
+        let tax = (subtotal - discounted_amount) * tax_rate
+        let total = subtotal + tax
+        //string templates
+        const reciept = 
+        `------- Reciept -------
+        Customer: ${customer_name} 
+        Item: ${grocery_item} 
+        Unit Price: $${money(unit_price)} 
+        Quantity ${quantity}
+        ------------------------
+        Subtotal: ${subtotal}
+        Discount: -$${discounted_amount}
+        Tax: ${tax}
+        Total: ${total}
+        ------------------------
+        Cash: ${cash}
+        Change: ${cash - total}`;
+        alert(reciept)
     }
 }
 
